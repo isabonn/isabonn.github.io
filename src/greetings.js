cfe90e2e0bc384ec053c3f9328905bb8e73666b3
Omid Najaee <o.najaee@gmail.com>
2018-02-09 00:24:01 +0100

    enable absolute module paths

--- src/greetings.js
+++ src/greetings.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 const Greetings = () => (
 	<h1>Greetings</h1>
 );
--- src/layouts/root/index.js
+++ src/layouts/root/index.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
-// import logo from 'assets/images/logo.svg';
+import logo from 'assets/images/logo.svg';
 import Greetings from 'greetings';
 import './Root.css';
-
-console.log(Greetings);
 
 class Root extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img  className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to ISA homepage</h1>
+			<img  src={logo} className="App-logo" alt="logo" />
+		<h1 className="App-title">Welcome to ISA homepage</h1>
         </header>
+        <Greetings />
       </div>
     );
   }
